fe4b2fa5e0e18ba60c596ed7558fc6a1180e6d27
ecupaio <ed.cupaioli@arcadiapower.com>
2020-02-21 17:00:46 -0500

    removed search plugin

--- _site/js/site_functions.js
+++ _site/js/site_functions.js
@@ -48,21 +48,14 @@ $(function() {
       $('.vid-overlay').addClass('min');
     }
   });
-  var searchResult = '<div class="search-result" data-artist="{artist}">'+
-                        '<h3>{artist}</h3>'+
-                        '<img src="https://img.youtube.com/vi/{freestyle}/mqdefault.jpg"/>'+
-                        '<div class="result-videos">'+
-                          '<div class="vid-link freestyle" onclick="playVid()" data-vid="{freestyle}">Freestyle</div>'+
-                          '<div class="vid-link" onclick="playVid()" data-vid="{cypher}">Cypher</div>'+
-                        '</div>'+
-                     '</div>';
-  var sjs = SimpleJekyllSearch({
-      searchInput: document.getElementById('search-input'),
-      resultsContainer: document.getElementById('results-container'),
-      json: '/search.json',
-      searchResultTemplate: searchResult,
-      templateMiddleware: refineResults()
-  });
+
+  // var sjs = SimpleJekyllSearch({
+  //     searchInput: document.getElementById('search-input'),
+  //     resultsContainer: document.getElementById('results-container'),
+  //     json: '/search.json',
+  //     searchResultTemplate: searchResult,
+  //     templateMiddleware: refineResults()
+  // });
   function refineResults() {
     $('.search-result').each(function() {
       $(this).find('.freestyle[data-vid="{freestyle}"]').addClass('hidden');
@@ -75,14 +68,51 @@ $(function() {
   });
   $('#search-form .search-input-box').on('input',function(e) {
     e.preventDefault();
-    var query = $('#search-input').val();
+    var query = $('#search-input').val().toLowerCase();
     if (query.length >= 2) {
-      sjs.search(query);
-      $('.search-result').each(function() {
-          $(this).find('.freestyle[data-vid="{freestyle}"]').addClass('hidden');
-      });
-      $('.search-result').remove();
+      //sjs.search(query);
+      $.ajax({
+        url: '/search.json',
+        success: function(data) {
+          $('.search-result').remove();
+          $.each(data, function(i,obj) {
+            var artist = obj.artist;
+            var artistLc = obj.artist.toLowerCase();
+            var freestyle = obj.freestyle;
+            var cypher = obj.cypher;
+            var searchResult = '<div class="search-result" data-artist="'+artist+'">'+
+                                  '<h3>'+artist+'</h3>'+
+                                  '<div class="artist-img">'+
 
+                                  '</div>'+
+                                  '<div class="result-videos">'+
+
+                                  '</div>'+
+                               '</div>';
+            if (artistLc.indexOf(query) > -1) {
+              if ($('.search-result[data-artist="'+artist+'"]').length < 1) {
+                $('#results-container').append(searchResult);
+                if (freestyle.length > 0) {
+                  $('.search-result[data-artist="'+artist+'"] .artist-img').append('<img src="https://img.youtube.com/vi/'+freestyle+'/mqdefault.jpg" class="artist-thumb" alt="">');
+                  $('.search-result[data-artist="'+artist+'"] .result-videos').append('<div class="vid-link freestyle" data-vid="'+freestyle+'">Freestyle</div>');
+                  playVid();
+                }
+                if (cypher.length > 0) {
+                  $('.search-result[data-artist="'+artist+'"] .result-videos').append('<div class="vid-link" data-vid="'+cypher+'">Cypher</div>');
+                  playVid();
+                }
+              }
+            } else {
+
+            }
+          });
+        }, error: function(data) {
+          console.log('error');
+        }
+      });
+    }
+    if (query.length < 2) {
+      $('.search-result').remove();
     }
   });
   $('#search-toggle').click(function() {
